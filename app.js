072799d99060fb41550f1599acf92e127a760896
Achyuth Reddy <ncsachyuth@gmail.com>
2020-10-15 23:44:23 +0530

    added the morgan middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const connectDB = require('./config/db')
+
 
 //load config
 dotenv.config( { path: './config/config.env'})
@@ -9,6 +11,11 @@ dotenv.config( { path: './config/config.env'})
 connectDB()
 
 const app = express()
+
+// logging 
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
 
 const PORT = process.env.PORT || 5000
 
